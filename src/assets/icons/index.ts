e218f8223bf157b75e78709cbd1f293b33a00a0c
Kitani Islam <slmkitani@gmail.com>
2023-12-27 19:30:40 +0800

    [FEAT] Icon Component + SVG's assets

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,22 @@
-export type TIcons = "Add";
+export type TIcons =
+  | "Add"
+  | "Moon"
+  | "MoonFill"
+  | "Sunny"
+  | "SunnyFill"
+  | "Menu"
+  | "LogoOutline"
+  | "Mail"
+  | "LogIn"
+  | "LogOut";
+
 export { default as Add } from "./Add.svg";
+export { default as LogIn } from "./LogIn.svg";
+export { default as LogOut } from "./LogOut.svg";
+export { default as LogoOutline } from "./LogoOutline.svg";
+export { default as Mail } from "./Mail.svg";
+export { default as Menu } from "./Menu.svg";
+export { default as Moon } from "./Moon.svg";
+export { default as MoonFill } from "./MoonFill.svg";
+export { default as Sunny } from "./Sunny.svg";
+export { default as SunnyFill } from "./SunnyFill.svg";
---
+++ src/components/atoms/Icon/Icon.styles.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+interface IconContainerProps {
+  pressable?: boolean;
+  disabled?: boolean;
+  padding?: number;
+  isAbsolutePosition?: boolean;
+}
+
+export const IconContainer = styled.div<IconContainerProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: ${({ pressable, padding }) =>
+    typeof padding === "number" ? `${padding}px` : pressable ? "8px" : "0px"};
+  ${({ isAbsolutePosition }) =>
+    isAbsolutePosition &&
+    `
+    position: absolute;
+    right: 0;
+    padding: 0;
+    `};
+`;
